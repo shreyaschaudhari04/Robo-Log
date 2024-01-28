0777a5916cbf81a6241016de2be2b4da6bca90ae
shreyaschaudhari04 <shreyasrchaudhari@gmail.com>
2024-01-28 21:40:45 +0530

    Initial Commit

---
+++ src/robots.js
@@ -0,0 +1,72 @@
+export const robots = [
+{
+    id:1,
+    name: 'Graham Welsh',
+    username: 'Graham1',
+    email:'graham.welsh2@gmail.com'
+},
+
+{
+    id:2,
+    name: 'Timothy West',
+    username: 'Timoth09',
+    email:'timothy2west@gmail.com'
+},
+
+{
+    id:3,
+    name: 'Tiffany Beck',
+    username: 'Tiffany45',
+    email:'tiffany.beck03@gmail.com'
+},
+
+{
+    id:4,
+    name: 'Raju Prasad',
+    username: 'Raju009',
+    email:'rajuprasad04@gmail.com'
+},
+
+{
+    id:5,
+    name: 'Pankaj Tripathi',
+    username: 'Pankaj04',
+    email:'pankajt2@gmail.com'
+},
+
+{
+    id:6,
+    name: 'Akshay Kumar',
+    username: 'Akshay',
+    email:'kumar.aksha01@gmail.com'
+},
+
+{
+    id:7,
+    name: 'Tiger Shroff',
+    username: 'Tiger004',
+    email:'tiger.shroff9@gmail.com'
+},
+
+{
+    id:8,
+    name: 'Rajkumar Rao',
+    username: 'Rajkumar09',
+    email:'rajrao@gmail.com'
+},
+
+{
+    id:9,
+    name: 'Kartik Aaryan',
+    username: 'Kartik09',
+    email:'kiki07@gmail.com'
+},
+
+{
+    id:10,
+    name: 'Ranveer Singh',
+    username: 'Ranveer69',
+    email:'ranveers69@gmail.com'
+},
+
+]
